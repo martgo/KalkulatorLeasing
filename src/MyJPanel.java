ad8f384dd846bc8a1e2d38f8d1369358b32d6fa5
martgo <martgo@op.pl>
2021-05-02 12:27:03 +0200

    Changing MyJPanel class

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -10,20 +10,41 @@ import java.sql.SQLException;
 public class MyJPanel extends JPanel implements ActionListener {
         public MyJPanel(){
             setLayout(null);
-
+//cars
             JLabel calculator = new JLabel("Kalkulator opłat leasingowych dotyczących samochodu osobowego");
             calculator.setBounds(150, 20, 500, 30);
             add(calculator);
-            JLabel carBrand = new JLabel("Samochód:");
-            carBrand.setBounds(90, 50, 100, 30);
+            JLabel carBrand = new JLabel("Samochód");
+            carBrand.setBounds(50, 50, 100, 30);
             add(carBrand);
             String calculatorPosition []={"X","X", "X","Inne"};
             JList opis = new JList(calculatorPosition);
-            opis.setBounds(100, 75, 250, 100);
+            opis.setBounds(80, 75, 250, 100);
             add(opis);
             JButton wybierz = new JButton("Wybierz");
-            wybierz.setBounds(270, 175, 90, 30);
+            wybierz.setBounds(200, 175, 90, 30);
             add(wybierz);
+
+            //data
+            JLabel textData = new JLabel("Dane");
+            textData.setBounds(50, 200, 200, 30);
+            add(textData);
+            JLabel textFirm = new JLabel("Marka:");
+            textFirm.setBounds(50, 225, 60, 30);
+            add(textFirm);
+            String textName = textFirm.getText();
+            JTextField textCarFirm = new JTextField();
+            textCarFirm.setBounds(100, 225, 200, 30);
+            add(textCarFirm);
+
+            JLabel carType = new JLabel("Typ pojazdu");
+            carType.setBounds(50, 270, 100, 30);
+            add(carType);
+
+            String[] selectType ={"Osobowy","Inny"};
+            JComboBox miecze =new JComboBox(selectType);
+            miecze.setBounds(130, 270,120,30);
+            add(miecze);
         }
 
         @Override
@@ -33,6 +54,7 @@ public class MyJPanel extends JPanel implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
+
 
         }
     }
