262361fd0d180f9ea9d596c720148457002e65dd
martgo <martgo@op.pl>
2021-05-02 11:31:13 +0200

    Adding MyJPanel class

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         JFrame window = new JFrame("Kalkulator opłat Leasingowych");
 
-//        MyJPanel panel = new MyJPanel();
-//        window.add(panel);
+        MyJPanel panel = new MyJPanel();
+        window.add(panel);
 
         window.setLocation(200,  100);
-//		window.setSize(500,500);
+		window.setSize(500,500);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
         window.pack();
