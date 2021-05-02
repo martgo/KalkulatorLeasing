84aac3d6cba32e72e15e75417e945951f39840d5
martgo <martgo@op.pl>
2021-05-02 12:17:14 +0200

    Changing MyJPanel class

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -11,26 +11,19 @@ public class MyJPanel extends JPanel implements ActionListener {
         public MyJPanel(){
             setLayout(null);
 
-            JLabel kalkulator = new JLabel("Kalkulator opłat leasingowych dotyczących samochodu osobowego");
-            kalkulator.setBounds(150, 20, 500, 30);
-            add(kalkulator);
-            String kalkulatorPozycje []={"X","X", "X","X"};
-            JList opis = new JList(kalkulatorPozycje);
+            JLabel calculator = new JLabel("Kalkulator opłat leasingowych dotyczących samochodu osobowego");
+            calculator.setBounds(150, 20, 500, 30);
+            add(calculator);
+            JLabel carBrand = new JLabel("Samochód:");
+            carBrand.setBounds(90, 50, 100, 30);
+            add(carBrand);
+            String calculatorPosition []={"X","X", "X","Inne"};
+            JList opis = new JList(calculatorPosition);
             opis.setBounds(100, 75, 250, 100);
             add(opis);
-            JLabel textRejestracja = new JLabel("Inne");
-            textRejestracja.setBounds(100, 200, 200, 30);
-            add(textRejestracja);
-            JLabel nazwa = new JLabel("Nazwa:");
-            nazwa.setBounds(10, 225, 60, 30);
-            add(nazwa);
-
             JButton wybierz = new JButton("Wybierz");
-            wybierz.setBounds(2, 275, 90, 30);
+            wybierz.setBounds(270, 175, 90, 30);
             add(wybierz);
-            JList wybieranie = new JList();
-            wybieranie.setBounds(100, 275, 250, 100);
-            add(wybieranie);
         }
 
         @Override
